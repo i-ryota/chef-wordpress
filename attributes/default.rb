f70a8d57945ac7fcf32c4660167725fe9be72ee7
i-ryota <ishii0524@gmail.com>
2013-11-29 09:46:16 +0000

    draft

---
+++ attributes/default.rb
@@ -0,0 +1,19 @@
+default['mysql']['db'] = "wp"
+default['mysql']['user'] = 'wp_user'
+default['mysql']['pwd'] = '1234'
+
+default['wordpress']['alias'] = 'wordpress'
+default['wordpress']['insdir'] = '/var/www/'
+default['wordpress']['home'] = '/var/www/wordpress/'
+default['wordpress']['dbhost'] = 'localhost'
+default['wordpress']['tableprefix'] = 'wp_'
+
+
+default['wordpress']['auth_key'] = 'R1caxAAkrFkkbZLc9yQy61RwE5QzASnIOGwogDmfWAfq2KKAC9G561j724qVKffU'
+default['wordpress']['secure_auth_key'] = '7IAXFIJw2BrGqbtMtI2LYHvA6yS2eIJZUVhUm6wmsT9XsyYEXazTUxZg5y1mWwHJ'
+default['wordpress']['logged_in_key'] = 'KYO720dHTJMHNScPC7grji9T27f23XHnxguSyLu91YSAzA7cP8lS9V9cWM72XNJp'
+default['wordpress']['nonce_key'] = 'X0unRs40D8GELttoIFaN1iG7HS3hyaLlE3gNij3YG3Z3Nf3TzVOKEyAXJwRlocT7'
+default['wordpress']['auth_salt'] = '0vBGGGLibZaDWe1o4mBiCWDkmRL5AbJJNZkoDkPo1AC4GvGh440nKYS8J4gVe9Ot'
+default['wordpress']['secure_auth_salt'] = 'GOBSUFYuktIsMAqCD07IFC2DmDbCMZ0tPObn5kRBnIYsMsnGT75sBTZd6PD8EPbj'
+default['wordpress']['logged_in_salt'] = 'w061JwkgPataHb68o1IVVgcn1tf9D478xVr7Am0l6VuupnlA2Aa49sfTCzze2l1e'
+default['wordpress']['nonce_salt'] = '5fYuLIuaY69JW0YEnrJDrdzk2KDbxu7XjixIy8cqQOJSx9jmV8FTSCfIwck1mDUs'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,104 @@
 #
-# Cookbook Name:: base
+# Cookbook Name:: test
 # Recipe:: default
 #
 # Copyright 2013, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
-#
+
+# install package
+%w{
+ httpd
+ mysql-server
+ php
+ php-mysql
+}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+# overrite php.ini
+cookbook_file "/etc/php.ini" do
+  source "php.ini"
+  owner "root"
+  group "root"
+  mode 0644
+end
+
+# overwite httpd.conf
+template "/etc/httpd/conf/httpd.conf" do
+  source "httpd.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+end
+
+# service setting httpd
+service "httpd" do
+  supports [:restart, :reload, :status]
+  action :enable
+end
+
+# service setting mysqld
+service "mysqld" do
+  supports [:restart, :reload, :status]
+  action [:enable, :start]
+end
+
+# create wordpress DB
+execute "create database" do
+  command "/usr/bin/mysqladmin -u root create #{node['mysql']['db']}"
+  not_if "/usr/bin/mysql -u root -e 'show databases;' | grep #{node['mysql']['db']}"
+end
+
+# copy SQL File to /tmp
+template "/tmp/grants.sql" do
+  source "grants.sql.erb"
+  owner "root"
+  group "root"
+  mode 0600
+end
+
+# create DB user
+execute "create database user" do
+  command "/usr/bin/mysql -u root < /tmp/grants.sql"
+end
+
+# get wordress
+remote_file "/tmp/wordpress.tar.gz" do
+  source "http://ja.wordpress.org/wordpress-3.7.1-ja.tar.gz"
+  owner "root"
+  group "root"
+  mode 0644
+  action :create_if_missing
+end
+
+# install wordpress
+execute "install wordpress" do
+  command "tar zxvf /tmp/wordpress.tar.gz -C #{node['wordpress']['insdir']}"
+  not_if{::Dir.exists?("#{node['wordpress']['home']}")}
+end
+
+directory "#{node['wordpress']['home']}" do
+  owner "apache"
+  group "apache"
+end
+
+# copy initial setting
+template "#{node['wordpress']['home']}wp-config.php" do
+  source "wp-config.php.erb"
+  owner "apache"
+  group "apache"
+  mode 0666
+end
+
+service "httpd" do
+  action :restart
+end
+
+service "iptables" do
+  action :stop
+end
+
+
